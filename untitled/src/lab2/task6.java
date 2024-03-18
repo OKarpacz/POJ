47a7338663d64a205c3d0b46390bb0fd38ce0b1e
Olgierd <olgierdkarpacz2000@gmail.com>
2024-03-18 08:45:16 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	untitled/src/lab2/task2.java

--- untitled/src/lab2/task6.java
+++ untitled/src/lab2/task6.java
@@ -1,7 +1,24 @@
 package lab2;
-
+import java.util.Scanner;
 public class task6 {
-    public static void main (String[] args){
-System.out.println("hELLO");
+    private static int NWD(int a, int b){
+        int c = (a>b) ? a%b : b%a;
+
+        if(c == 0)
+            return (a>b) ? b : a;
+            return a>b ? NWD(c,b) : NWD(c,a);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj pierwszą liczbę:");
+        int a = scanner.nextInt();
+
+        System.out.print("Podaj drugą liczbę:");
+        int b = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println("Najwiekszym wspolnim dzielnikiem liczb " + a +" i " + b+ " to: " + NWD(a,b));
     }
 }
