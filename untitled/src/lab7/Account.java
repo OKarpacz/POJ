e6420bc5c3a02126fd8a3af27c80ed9ad99076bd
OKarpacz <olgierdkarpacz2000@gmail.com>
2024-05-05 21:44:10 +0200

    lab7 i lab6(naprawiennie error)

---
+++ untitled/src/lab7/Account.java
@@ -0,0 +1,18 @@
+public class Account {
+    private String accountName;
+    private double balance;
+
+    public Account(String accountName, double initialBalance) {
+        this.accountName = accountName;
+        this.balance = initialBalance;
+    }
+
+    public int withdraw(int amount) {
+        balance-=amount;
+        return amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
